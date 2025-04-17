feec3b6ba698349118f81ef592efd932a16b5f29
chloeandersen-dev <chloe@andersens.com>
2025-04-17 13:21:40 -0700

    Chloe A- Issue #20: Create database table for RecommendationRequest

---
+++ src/main/java/edu/ucsb/cs156/example/entities/RecommendationRequest.java
@@ -0,0 +1,30 @@
+package edu.ucsb.cs156.example.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
+
+/**
+ * This is a JPA entity that represents a UCSB Recommendation Requests,
+ *  i.e. an entry that comes from the UCSB API for recommendation request data.
+ */
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity(name = "RecommendationRequest")
+public class RecommendationRequest {
+  @Id
+  private long id;
+  private String requesterEmail;
+  private String professorEmail;
+  private String explanation;
+  private LocalDateTime dateRequested;
+  private LocalDateTime dateNeeded;
+  private Boolean done;
+}
