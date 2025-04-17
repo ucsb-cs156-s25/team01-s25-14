cfc6e0afa895e435ae093b3ad7dd5c6f47ae0d43
chloeandersen-dev <chloe@andersens.com>
2025-04-17 13:41:11 -0700

    Modifying RecommendationRequest.json to match the correct tableName of the @Entity and @Respository classes

--- src/main/java/edu/ucsb/cs156/example/entities/RecommendationRequest.java
+++ src/main/java/edu/ucsb/cs156/example/entities/RecommendationRequest.java
@@ -7,7 +7,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import java.time.LocalDateTime;
-
+import java.util.Date;
 /**
  * This is a JPA entity that represents a UCSB Recommendation Requests,
  *  i.e. an entry that comes from the UCSB API for recommendation request data.
