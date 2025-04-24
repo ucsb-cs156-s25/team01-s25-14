95dcfde8f80645ebec0c398957d737a3ade44a22
Karen Yuan <karenyuan@ucsb.edu>
2025-04-24 00:56:35 -0700

    ky - add controller method for GET by id

--- src/main/java/edu/ucsb/cs156/example/controllers/ArticlesController.java
+++ src/main/java/edu/ucsb/cs156/example/controllers/ArticlesController.java
@@ -94,5 +94,22 @@ public class ArticlesController extends ApiController {
         return savedArticle;
     }
 
+    /**
+     * Get a single article by id
+     * 
+     * @param id the id of the article
+     * @return a Article
+     */
+    @Operation(summary= "Get a single article")
+    @PreAuthorize("hasRole('ROLE_USER')")
+    @GetMapping("")
+    public Articles getById(
+            @Parameter(name="id") @RequestParam Long id) {
+        Articles article = articlesRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException(Articles.class, id));
+
+        return article;
+    }
+
 
 }
