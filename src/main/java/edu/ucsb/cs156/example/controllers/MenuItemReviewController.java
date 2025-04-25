c726994d882b262b1999b15be51428932fa33b0d
Shruthi Unnithan <ssanthoshunnithan@ucsb.edu>
2025-04-24 18:41:50 -0700

    delete method implemented (without tests)

--- src/main/java/edu/ucsb/cs156/example/controllers/MenuItemReviewController.java
+++ src/main/java/edu/ucsb/cs156/example/controllers/MenuItemReviewController.java
@@ -136,4 +136,22 @@ public class MenuItemReviewController extends ApiController{
         return menuItemReview;
     }
 
+    /**
+     * Delete a MenuItemReview
+     * 
+     * @param id the id of the review to delete
+     * @return a message indicating the review was deleted
+     */
+    @Operation(summary= "Delete a MenuItemReview")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @DeleteMapping("")
+    public Object deleteMenuItemReview(
+            @Parameter(name="id") @RequestParam Long id) {
+        MenuItemReview menuItemReview = menuItemReviewRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException(MenuItemReview.class, id));
+
+        menuItemReviewRepository.delete(menuItemReview);
+        return genericMessage("MenuItemReview with id %s deleted".formatted(id));
+    }
+
 }
